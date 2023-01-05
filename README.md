1adc585a63459d57e616db4faad29af40952e532
James Smith <james.smith9113@gmail.com>
2023-01-04 21:05:08 -0500

    Overhauling website

--- README.md
+++ README.md
@@ -1,99 +1,10 @@
-# Public Web Templates
+# Personal Website
 
-Basic templates for individual user pages in your public_www directory at the Flatiron Institute.
+Serve locally with:
 
-## Getting Started
-
-Each user can create their own public, static website, hosted at https://users.flatironinstitute.org/~username/. For files on popeye, sites are hosted at https://sdsc-users.flatironinstitute.org/~username/.
-
-Simply create a directory called `public_www` in your Linux home directory (Mac users should first ssh to a Linux host, like "rusty").
-
-```zsh
-mkdir ~/public_www
+```shell
+python3 -m http.server 8888
 ```
-
-Using your preferred editor, clone this repository into the new directory and move it one level up. 
-
-```zsh
-git clone <this-repo>
-mv public_www_template/* public_www_template/.* .
-```
-
-This file and any others you place in this directory are automatically published. Symlinks are followed, so if you want to host large datasets, you should place them in /mnt/ceph and symlink from there (both the symlink and the target must be owned by you: you may not host other users' data).
-
-Note: Any files and directories you wish to be published must be publicly readable (chmod a+rX).
-
-## Template
-
-The `index.html` file contains a stubbed out personal website template. Comments labeled with `TODO` have been provided in the file for easy search and replace.
-
-```html
-<!-- TODO: Add your citations  -->
-```
-
-If you wish to have both a thumbnail (for fast loading) and a larger image (for detail) please follow the convention of the first example project.
-
-```html
-<article class="col-6 col-12-xsmall work-item">
-    <a href="images/fulls/02.jpg" class="image fit thumb">
-        <img src="images/thumbs/02.jpg" alt="gray mist" />
-    </a>
-</article>
-```
-
-
-### Mathjax
-
-Mathjax 3.0 has been included as a courtesy. Feel free to delete if you are not using.
-
-
-### Icons
-
-This page uses Font Awesome vector icons for social media. Additional logos can be found on the [Font Awesome](https://fontawesome.com/v5.15/icons?d=gallery&p=2&m=free) website and can be added using the `icon` and `fa-*` css classes.
-
-```html
-<a href="#" class="icon fa-instagram">
-    <span class="label">Instagram</span>
-</a>
-```
-
-
-### Folder Structure
-
-    .
-    ├── assets
-    ├── images
-    │   ├── fulls
-    │   ├── thumbs
-    │   ├── avatar-placeholder.jpg
-    │   └── avatar.jpg
-    ├── pdfs
-    │   ├── fakecv.pdf
-    │   └── fakepaper.jpg
-    ├── index.html
-    ├── LICENSE
-    └── README.md
-
-
-## Resources
-
-
-### Additional Documentation
-
-- [Public Web Hosting at Flatiron Institute][wiki]
-- [HTML Basics][moz-docs]
-- [HTML Reference][moz-refs]
-- [CSS Basics][css-basics]
-- [Javascript Basics][js-basics]
-- [Mathjax3 Docs][mathjax]
-
-[wiki]: https://docs.simonsfoundation.org/index.php/PublicWWW
-[moz-docs]: https://developer.mozilla.org/en-US/docs/Web/HTML
-[moz-refs]: https://developer.mozilla.org/en-US/docs/Web/HTML/Reference
-[css-basics]: https://developer.mozilla.org/en-US/docs/Web/CSS
-[js-basics]: https://developer.mozilla.org/en-US/docs/Web/JavaScript
-[mathjax]: https://docs.mathjax.org/en/latest/index.html
-
 
 ## License
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,166 +1,123 @@
 /*
-	Public User Page Template
-    Flatiron Institute
-	flatironinstitute.org
+	Stellar by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function ($) {
-  var $window = $(window),
-    $body = $("body"),
-    $header = $("#header"),
-    $footer = $("#footer"),
-    $main = $("#main"),
-    $nav = $('#nav'),
-    $year = $('#year'),
-    $center = $('#center'),
-    settings = {
-      // Parallax background effect?
-      parallax: true,
+(function($) {
 
-      // Parallax factor (lower = more intense, higher = less intense).
-      parallaxFactor: 20,
-    };
+	var	$window = $(window),
+		$body = $('body'),
+		$main = $('#main');
 
-  // Breakpoints.
-  breakpoints({
-    xlarge: ["1281px", "1800px"],
-    large: ["981px", "1280px"],
-    medium: ["737px", "980px"],
-    small: ["481px", "736px"],
-    xsmall: [null, "480px"],
-  });
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
 
-  // Play initial animations on page load.
-  $window.on("load", function () {
-    // Add Date
-    const today = new Date();
-    $year.textContent += (`, ${today.getFullYear()}`);
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
-    window.setTimeout(function () {
-      $body.removeClass("is-preload");
-    }, 100);
-  });
+	// Nav.
+		var $nav = $('#nav');
 
-  // Touch?
-  if (browser.mobile) {
-    // Turn on touch mode.
-    $body.addClass("is-touch");
+		if ($nav.length > 0) {
 
-    // Height fix (mostly for iOS).
-    window.setTimeout(function () {
-      $window.scrollTop($window.scrollTop() + 1);
-    }, 0);
-  }
+			// Shrink effect.
+				$main
+					.scrollex({
+						mode: 'top',
+						enter: function() {
+							$nav.addClass('alt');
+						},
+						leave: function() {
+							$nav.removeClass('alt');
+						},
+					});
 
-  // Footer.
-  breakpoints.on("<=medium", function () {
-    $footer.insertAfter($main);
-  });
+			// Links.
+				var $nav_a = $nav.find('a');
 
-  breakpoints.on(">medium", function () {
-    $footer.appendTo($header);
-  });
+				$nav_a
+					.scrolly({
+						speed: 1000,
+						offset: function() { return $nav.height(); }
+					})
+					.on('click', function() {
 
-  // Header.
+						var $this = $(this);
 
-  // Parallax background.
+						// External link? Bail.
+							if ($this.attr('href').charAt(0) != '#')
+								return;
 
-  // Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-  if (browser.name == "ie" || browser.mobile) settings.parallax = false;
+						// Deactivate all links.
+							$nav_a
+								.removeClass('active')
+								.removeClass('active-locked');
 
-  if (settings.parallax) {
-    breakpoints.on("<=medium", function () {
-      $window.off("scroll.strata_parallax");
-      $header.css("background-position", "");
-    });
+						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+							$this
+								.addClass('active')
+								.addClass('active-locked');
 
-    breakpoints.on(">medium", function () {
-      $header.css("background-position", "left 0px");
+					})
+					.each(function() {
 
-      $window.on("scroll.strata_parallax", function () {
-        $header.css(
-          "background-position",
-          "left " +
-            -1 * (parseInt($window.scrollTop()) / settings.parallaxFactor) +
-            "px"
-        );
-      });
-    });
+						var	$this = $(this),
+							id = $this.attr('href'),
+							$section = $(id);
 
-    $window.on("load", function () {
-      $window.triggerHandler("scroll");
-    });
-  }
+						// No section for this link? Bail.
+							if ($section.length < 1)
+								return;
 
-  // Nav.
-  var $nav_a = $nav.find('a');
+						// Scrollex.
+							$section.scrollex({
+								mode: 'middle',
+								initialize: function() {
 
-  $nav_a
-    .addClass('scrolly')
-    .on('click', function() {
+									// Deactivate section.
+										if (browser.canUse('transition'))
+											$section.addClass('inactive');
 
-      var $this = $(this);
+								},
+								enter: function() {
 
-      // External link? Bail.
-        if ($this.attr('href').charAt(0) != '#')
-          return;
+									// Activate section.
+										$section.removeClass('inactive');
 
-      // Deactivate all links.
-        $nav_a.removeClass('active');
+									// No locked links? Deactivate all links and activate this section's one.
+										if ($nav_a.filter('.active-locked').length == 0) {
 
-      // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-        $this
-          .addClass('active')
-          .addClass('active-locked');
+											$nav_a.removeClass('active');
+											$this.addClass('active');
 
-    })
-    .each(function() {
+										}
 
-      var	$this = $(this),
-        id = $this.attr('href'),
-        $section = $(id);
+									// Otherwise, if this section's link is the one that's locked, unlock it.
+										else if ($this.hasClass('active-locked'))
+											$this.removeClass('active-locked');
 
-      // No section for this link? Bail.
-        if ($section.length < 1)
-          return;
+								}
+							});
 
-      // Scrollex.
-        $section.scrollex({
-          mode: 'default',
-          top: '5vh',
-          bottom: '5vh',
-          initialize: function() {
+					});
 
-            // Deactivate section.
-              $section.addClass('inactive');
+		}
 
-          },
-          enter: function() {
+	// Scrolly.
+		$('.scrolly').scrolly({
+			speed: 1000
+		});
 
-            // Activate section.
-              $section.removeClass('inactive');
-
-            // No locked links? Deactivate all links and activate this section's one.
-              if ($nav_a.filter('.active-locked').length == 0) {
-
-                $nav_a.removeClass('active');
-                $this.addClass('active');
-
-              }
-
-            // Otherwise, if this section's link is the one that's locked, unlock it.
-              else if ($this.hasClass('active-locked'))
-                $this.removeClass('active-locked');
-
-          }
-        });
-
-    });
-
-    // Center Color
-    let root = document.documentElement;
-    $center.on("change", function () {
-      $val = $center.find(":selected").val();
-      root.style.setProperty('--accent', $val);
-    });
 })(jQuery);
